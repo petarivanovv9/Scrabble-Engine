643b0dbef2cf2d5ee6ae3fd90a1cf4c4860e43ef
Petar Ivanov <pepincho95@gmail.com>
2017-04-17 12:56:40 +0300

    Add initial Cell class declaration.

--- Sources/Tile.swift
+++ Sources/Tile.swift
@@ -1,18 +1,18 @@
-import Foundation
+// import Foundation
+//
+// class Tile: CustomStringConvertible {
+//   let letter: Character
+//   let score: Int
+//
+//   init(letter: Character, score: Int = 0) {
+//       self.letter = letter
+//       self.score = score
+//   }
+//
+//   var description: String {
+//     return "\(letter) with \(score)"
+//   }
+// }
 
-class Tile: CustomStringConvertible {
-  let letter: Character
-  let score: Int
-
-  init(letter: Character, score: Int = 0) {
-      self.letter = letter
-      self.score = score
-  }
-
-  var description: String {
-    return "\(letter) with \(score)"
-  }
-}
-
-var A_Tile = Tile(letter: "A", score: 3)
-print(A_Tile)
+// var A_Tile = Tile(letter: "A", score: 3)
+// print(A_Tile)
